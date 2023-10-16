ed352e2b392ea7f2c8017c69b45b34b1b85eaf64
erinvmiller <erin.miller@wustl.edu>
2023-10-15 23:19:30 -0400

    Update lf2lambda_function.py

--- lambdafunctions/lf2lambda_function.py
+++ lambdafunctions/lf2lambda_function.py
@@ -5,6 +5,7 @@ from boto3.dynamodb.conditions import Key, Attr
 import boto3
 import json
 import logging
+import aws_requests_auth
 
 emailClient = boto3.client('ses', region_name='us-east-1')
 
@@ -12,6 +13,12 @@ sqs = boto3.client("sqs")
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 QUEUE_URL = "https://sqs.us-east-1.amazonaws.com/532016339310/ReservationRequestQueue"
+ES_HOST = "https://search-restaurant-mktfqcghh5c6w6qpfu6b24i7da.us-east-1.es.amazonaws.com/_dashboards"
+INDEX_PATH = "https://search-restaurant-mktfqcghh5c6w6qpfu6b24i7da.us-east-1.es.amazonaws.com/restaurants?pretty"
+region = 'us-east-1' 
+service = 'es'
+access_key='xxx'
+secret_key='yyy'
 
 def getSqsData():
     response = sqs.receive_message(
@@ -41,22 +48,42 @@ def extractMessageBody(messages):
         res.append(msg['Body'])
     return res[0]
 
-def generateEmailBody(qData):
+def generateEmailBody(qData, restaurantName):
     if qData != None:
-        return ("You reservation for table of " + qData['numberOfPeople'] + " at " + "______" + " is confirmed. Please find the details of the reservation below: " + "\n" + \
-            "Restaurant Name: " + "____" + "\n" +\
+        return ("You reservation for table of " + qData['numberOfPeople'] + " at " + restaurantName + " is confirmed. Please find the details of the reservation below: " + "\n" + \
+            "Restaurant Name: " + restaurantName + "\n" +\
             "Reservation Time: " + qData['diningTime'] + "\n" +\
             "\n" + "\n" +\
             "Enjoy your food!\n")
     else:
         return None
 
+def queryEs(qData):
+    auth = AWSRequestsAuth(aws_access_key=access_key,
+                              aws_secret_access_key=secret_key,
+                              aws_host=ES_HOST,
+                              aws_region=region,
+                              aws_service=service)
+    url = f'https://{es_host}/restaurants/_search'
+    query = {
+        "query": {
+            "match": {
+                "cuisine": qData['cuisine']
+            }
+        }
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    response = requests.post(url, auth=auth, headers=headers, json=query)
+    return response.json()['hits']['hits'][0]
+
 def lambda_handler(event, context):
     # TODO implement
     qData = getSqsData()
-    emailBod = generateEmailBody(qData)
-    print("emailBod = ", emailBod)
     if qData != None:
+        restaurantName = queryEs(qData)
+        emailBod = generateEmailBody(qData, restaurantName)
         response = emailClient.send_email(
             Destination={'ToAddresses': [qData['email']]},
             # Destination={'ToAddresses': ["aa10770@nyu.edu"]},
